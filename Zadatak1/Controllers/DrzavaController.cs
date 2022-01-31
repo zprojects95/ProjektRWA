09ed48dfc05dd323ac7fb1a52231f4a3a9f44399
zprojects95 <zprojects95@hotmail.com>
2022-01-31 16:38:10 +0100

    Add Drzava repository and controller

---
+++ Zadatak1/Controllers/DrzavaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Zadatak1.Repositories;
+
+namespace Zadatak1.Controllers
+{
+    public class DrzavaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            return Ok(DrzavaRepository.GetAll());
+        }
+    }
+}
