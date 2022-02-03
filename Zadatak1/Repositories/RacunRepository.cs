3ccd01f211f4d6bc7b8297d1656dd279be4cf24f
zprojects95 <zprojects95@hotmail.com>
2022-02-04 00:47:23 +0100

    Add Racun repository

---
+++ Zadatak1/Repositories/RacunRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Zadatak1.Repositories
+{
+    public class RacunRepository
+    {
+        public static DataSet ds { get; set; }
+        private static readonly string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        public static List<Racun> GetByKupacId(int id)
+        {
+            List<Racun> racuni = new List<Racun>();
+
+            ds = SqlHelper.ExecuteDataset(cs, "GetRacuniKupca", id);
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+
+                DateTime date = DateTime.Parse(row["DatumIzdavanja"].ToString());
+
+                racuni.Add(new Racun
+                {
+                    IDRacun = (int)row["IDRacun"],
+                    DatumIzdavanja = date,
+                    BrojRacuna = row["BrojRacuna"].ToString(),
+                    Komentar = row["Komentar"].ToString()
+                });
+            }
+            return racuni;
+        }
+    }
+}
