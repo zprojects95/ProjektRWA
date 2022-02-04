5458eed9e4751a1cde33d0be322ff4bedde51848
Andrija Ukošić-Mihajlik <aukosic@racunarstvo.hr>
2022-02-04 17:17:38 +0100

    Add Stavka repository

---
+++ Zadatak1/Repositories/StavkaRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ApplicationBlocks.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Zadatak1.Models;
+
+namespace Zadatak1.Repositories
+{
+    public class StavkaRepository
+    {
+        public static DataSet ds { get; set; }
+        private static readonly string cs = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        public static List<Stavka> GetStavkeByRacunId(int id)
+        {
+            List<Stavka> stavke = new List<Stavka>();
+
+            ds = SqlHelper.ExecuteDataset(cs, "GetStavkeRacuna", id);
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                stavke.Add(new Stavka
+                {
+                    BrojKreditneKartice = row["Broj Kreditne Kartice"].ToString(),
+                    Komercijalist = row["Komercijalist"].ToString(),
+                    Proizvod = row["Proizvod"].ToString(),
+                    Potkategorija = row["Potkategorija"].ToString(),
+                    Kategorija = row["Kategorija"].ToString()
+                });
+            }
+            return stavke;
+        }
+    }
+}
