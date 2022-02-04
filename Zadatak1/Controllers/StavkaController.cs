5c6a9d94a9b181d1692fad908ac5bc3c561d40ec
Andrija Ukošić-Mihajlik <aukosic@racunarstvo.hr>
2022-02-04 17:17:49 +0100

    Add Stavka controller

---
+++ Zadatak1/Controllers/StavkaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Zadatak1.Repositories;
+
+namespace Zadatak1.Controllers
+{
+    public class StavkaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetRacuniByKupacId(int id)
+        {
+            return Ok(StavkaRepository.GetStavkeByRacunId(id));
+        }
+    }
+}
