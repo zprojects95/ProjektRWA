12e019193ecede1e6c4a893e8c4cc0c12b483422
zprojects95 <zprojects95@hotmail.com>
2022-02-04 14:26:08 +0100

    Change layout and delete unused views

--- Zadatak1/Controllers/HomeController.cs
+++ Zadatak1/Controllers/HomeController.cs
@@ -18,18 +18,5 @@ namespace Zadatak1.Controllers
             return View(id);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
