b697786a26fa16450c9abacbc7f206e29d38a530
zprojects95 <zprojects95@hotmail.com>
2022-02-08 21:54:19 +0100

    Add Kategorija model

---
+++ Zadatak1/Models/Kategorija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zadatak1.Models
+{
+    public class Kategorija
+    {
+        public int IDKategorija { get; set; }
+        public string Naziv { get; set; }
+    }
+}
